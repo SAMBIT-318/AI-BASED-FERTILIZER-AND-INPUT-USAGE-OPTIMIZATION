11524226766e91dad6e62c5387eae565c467f24b
SAMBIT SWAIN <sambitswain318@gmail.com>
2026-09-26 00:58:55 +0530

    Change labelAngle from 360 to 0 in app.py

--- app.py
+++ app.py
@@ -1899,7 +1899,7 @@ elif st.session_state.step == 6:
                 axis=alt.Axis(
                     labelColor="#000000",
                     titleColor="#000000",
-                    labelAngle=360,
+                    labelAngle=0,
                     lableFontSize=12,
                     title="Stage",
                 ),
