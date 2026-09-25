5ef29fbbd7d0adb6cf7b321d4efacd590a60980d
SAMBIT SWAIN <sambitswain318@gmail.com>
2026-09-26 00:49:57 +0530

    Change label angle from 90 to 360 degrees

--- app.py
+++ app.py
@@ -1899,7 +1899,7 @@ elif st.session_state.step == 6:
                 axis=alt.Axis(
                     labelColor="#000000",
                     titleColor="#000000",
-                    labelAngle=90,
+                    labelAngle=360,
                     title="Stage",
                 ),
             ),
